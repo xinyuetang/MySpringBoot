af5c2ac13443b90cb7539c782e014f22c7cd605f
tidu <baomingfeng@vpgame.cn>
2021-05-02 22:58:42 +0800

    Initialize

--- fd-cms/start.sh
+++ fd-cms/start.sh
@@ -2,4 +2,4 @@
 
 CUR_DIR=$(cd "$(dirname "$0")";pwd)
 APPLICATION_PATH=$CUR_DIR/fd-cms-1.0.0.jar
-nohup -Dspring.profiles.active=dev -Denv=dev -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m  -Xms256m -Xmx512m -XX:+PrintGCDetails -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -jar $APPLICATION_PATH >/dev/null 2>&1 &
+nohup -Dspring.profiles.active=dev -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m  -Xms256m -Xmx512m -XX:+PrintGCDetails -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -jar $APPLICATION_PATH >/dev/null 2>&1 &
