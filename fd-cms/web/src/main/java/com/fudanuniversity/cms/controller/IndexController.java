9d347e0ca242ed97e5d170cbdf9c54c5d518bfdf
tidu <baomingfeng@vpgame.cn>
2021-05-03 02:34:37 +0800

    Initialize

---
+++ fd-cms/web/src/main/java/com/fudanuniversity/cms/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.fudanuniversity.cms.controller;
+
+import com.fudanuniversity.cms.commons.model.JsonResult;
+import com.fudanuniversity.cms.commons.util.DateExUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Date;
+
+/**
+ * Created by tidu at 2021-05-03 02:27:52
+ */
+@RestController
+public class IndexController extends BaseController {
+
+    @RequestMapping("/")
+    public JsonResult<?> index() {
+        Object greeting = Collections.singletonMap("datetime", DateExUtils.formatDatetimeMilli(new Date()));
+        return JsonResult.buildSuccess(greeting);
+    }
+}
---
+++ fd-cms/web/src/main/java/com/fudanuniversity/cms/controller/PlanController.java
@@ -0,0 +1,10 @@
+package com.fudanuniversity.cms.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tidu at 2021-05-03 02:26:12
+ */
+@RestController
+public class PlanController extends BaseController {
+}
