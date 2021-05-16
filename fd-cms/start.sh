12e1d7483137363dffdb152e0e8ec79c42afb2d3
tidu <baomingfeng@vpgame.cn>
2021-05-17 04:13:23 +0800

    docker环境支持

--- fd-cms/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CUR_DIR=$(cd "$(dirname "$0")";pwd)
-APPLICATION_PATH=$CUR_DIR/fd-cms-web-1.0.0.jar
-nohup -Dspring.profiles.active=dev -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m  -Xms1024m -Xmx1024m -XX:+PrintGCDetails -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -jar $APPLICATION_PATH >/dev/null 2>&1 &
--- fd-cms/web/src/main/java/com/fudanuniversity/cms/controller/IndexController.java
+++ fd-cms/web/src/main/java/com/fudanuniversity/cms/controller/IndexController.java
@@ -2,13 +2,15 @@ package com.fudanuniversity.cms.controller;
 
 import com.fudanuniversity.cms.commons.model.JsonResult;
 import com.fudanuniversity.cms.commons.util.DateExUtils;
+import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.net.InetAddress;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Xinyue.Tang at 2021-05-03 02:27:52
@@ -19,8 +21,10 @@ public class IndexController extends BaseController {
 
     @ApiOperation(value = "默认", notes = "默认Note")
     @GetMapping("/")
-    public JsonResult<?> index() {
-        Object greeting = Collections.singletonMap("datetime", DateExUtils.formatDatetimeMilli(new Date()));
+    public JsonResult<?> index() throws Exception {
+        Map<String, Object> greeting = Maps.newLinkedHashMap();
+        greeting.put("datetime", DateExUtils.formatDatetimeMilli(new Date()));
+        greeting.put("address", InetAddress.getLocalHost().getHostAddress());
         return JsonResult.buildSuccess(greeting);
     }
 }
